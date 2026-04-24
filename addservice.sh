81c5a9234385642fdc2cf938b0389252b8a43c55
shady <shadybradengithub@shady.icu>
2026-04-24 15:27:48 -0400

    update to new method

---
+++ addservice.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SERVERNAME=$1
+SERVICE=$2
+
+TOML_BLOCK=$(cat <<EOF
+
+##
+
+[[stack]]
+name = "$SERVICE-$SERVERNAME"
+tags = ["$SERVERNAME", "criticality: 1"]
+[stack.config]
+server = "$SERVERNAME"
+project_name = "$SERVICE-$SERVERNAME"
+linked_repo = "compose_main"
+run_directory = "$SERVICE"
+additional_env_files = ["/etc/komodo/domain.env"]
+env_file_path = "/etc/komodo/env/$SERVICE-$SERVERNAME.env"
+EOF
+)
+
+PLAYBOOK_ADDITION=$(cat <<EOF
+    - $SERVICE
+EOF
+)
+
+echo "$TOML_BLOCK" >> syncs/$SERVERNAME.toml
+echo "$PLAYBOOK_ADDITION" >> $SERVERNAME.yaml
